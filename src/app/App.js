8d5f05d3a2b55f4e0098630141656297a562b55b
Alex Thomas <hello@cawfree.com>
2020-07-23 21:40:06 +0100

    chore: add token serialization

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect, useCallback} from "react";
 import PropTypes from "prop-types";
 import TorusSdk from "@toruslabs/torus-direct-web-sdk";
+import Cookies from 'universal-cookie';
 
 const App = ({isServerSide, config}) => {
   const {
@@ -32,18 +33,6 @@ const App = ({isServerSide, config}) => {
     [sdk, setDidInit, isServerSide, setError],
   );
 
-  const shouldPostMessage = useCallback(
-    (e) => {
-      // https://github.com/react-native-community/react-native-webview/
-      if (window.ReactNativeWebView && window.ReactNativeWebView.postMessage) {
-        return window.ReactNativeWebView.postMessage(e);
-      }
-      console.warn('not posting message', e);
-      return undefined;
-    },
-    [],
-  );
-
   const shouldTriggerLogin = useCallback(
     () => {
       if (didInit) {
@@ -58,19 +47,14 @@ const App = ({isServerSide, config}) => {
               jwtParams,
             }),
           )
-          .then((userData) => {
-            // TODO: make configurable
-            if (!isServerSide) {
-              return shouldPostMessage(userData);
-            }
-            return undefined;
-          })
+          .then(results => new Cookies().set("torus", JSON.stringify(results), { path: "/" }))
           .catch(setError) && undefined;
       }
       return Promise.reject(new Error(`Not yet initialized!`)) && undefined;
     },
-    [didInit, sdk, verify, jwtParams, setError, shouldPostMessage],
+    [didInit, sdk, verify, jwtParams, setError],
   );
+
   return (
     <div>
       {(!error) ? (
@@ -81,8 +65,8 @@ const App = ({isServerSide, config}) => {
             />
           ) : (
             <button
-              onClick={() => shouldTriggerLogin()}
-              children="Open login."
+              onClick={shouldTriggerLogin}
+              children="Sign In"
             />
           )}
         </>
