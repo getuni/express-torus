21ea4ea40e65b9aa6bf4d52ba7aba6418fca74b7
Alex Thomas <hello@cawfree.com>
2020-07-23 12:59:27 +0100

    chore: abstract sdk initialization

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useCallback} from "react";
 import PropTypes from "prop-types";
 import TorusSdk from "@toruslabs/torus-direct-web-sdk";
 
@@ -11,7 +11,7 @@ const App = ({config}) => {
     loginToConnectionMap,
     verifierMap,
   } = config;
-  // TODO: props
+
   const [sdk] = useState(
     new TorusSdk({
       baseUrl,
@@ -20,37 +20,55 @@ const App = ({config}) => {
       network,
     }),
   );
+
+  const [didInit, setDidInit] = useState(false);
+
   useEffect(
-    () => {
-      sdk.init()
-        .then(
-          () => {
-            const selectedVerifier = "google";
-            const jwtParams = loginToConnectionMap[selectedVerifier] || {};
-            const { typeOfLogin, clientId, verifier } = verifierMap[selectedVerifier];
-            return sdk.triggerLogin({
-              typeOfLogin,
-              verifier,
-              clientId,
-              jwtParams,
-            });
-          },
-        )
-        .then(console.log)
-        .catch(console.error)
-    },
-    [sdk],
+    () => sdk.init()
+      .then(() => setDidInit(true))
+      .catch(console.error) && undefined,
+    [sdk, setDidInit],
+  );
+
+  const shouldTriggerLogin = useCallback(
+    selectedVerifier => Promise
+      .resolve()
+      .then(
+        () => {
+          if (!didInit) {
+            return Promise.reject(new Error("The sdk has not been initialized."));
+          }
+          return undefined;
+        }
+      )
+      .then(
+        () => {
+          const jwtParams = loginToConnectionMap[selectedVerifier] || {};
+          const { typeOfLogin, clientId, verifier } = verifierMap[selectedVerifier];
+          return sdk.triggerLogin({typeOfLogin, verifier, clientId, jwtParams});
+        },
+      ),
+    [sdk, didInit, loginToConnectionMap],
   );
   return (
     <div>
-      "hello from react file"
+      <button
+        onClick={() => shouldTriggerLogin("twitter")}
+      >
+        "hello from react file"
+      </button>
     </div>
   );
 };
 
 App.propTypes = {
   config: PropTypes.shape({
-
+    baseUrl: PropTypes.string.isRequired,
+    proxyContractAddress: PropTypes.string.isRequired,
+    network: PropTypes.string.isRequired,
+    enableLogging: PropTypes.bool,
+    loginToConnectionMap: PropTypes.shape({}).isRequired,
+    verifierMap: PropTypes.shape({}).isRequired,
   }).isRequired,
 };
 
