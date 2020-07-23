4777c281f47c8f6461575847cb83675fbb22cac5
Alex Thomas <hello@cawfree.com>
2020-07-23 12:50:53 +0100

    chore: update example

--- src/index.js
+++ src/index.js
@@ -7,15 +7,24 @@ import {compile} from "handlebars";
 
 import App from "./app/App";
 
-const app = () => (req, res, next) => Promise
+const app = ({
+  baseUrl,
+  enableLogging,
+  proxyContractAddress,
+  network,
+  verifierMap,
+  loginToConnectionMap,
+}) => (req, res, next) => Promise
   .resolve()
   .then(
-    () => {
+    () => { 
       const config = Object.freeze({
-        baseUrl: "http://localhost:3000/serviceworker/",
-        enableLogging: true,
-        proxyContractAddress: "0x4023d2a0D330bF11426B12C6144Cfb96B7fa6183", // details for test net
-        network: "ropsten", // details for test net
+        baseUrl,
+        enableLogging,
+        proxyContractAddress,
+        network,
+        loginToConnectionMap,
+        verifierMap,
       });
       // TODO: pass children for custom render
       const container = renderToString(
@@ -47,8 +56,8 @@ const app = () => (req, res, next) => Promise
   )
   .catch(next);
 
-export const torus = () => express()
+export const torus = (...args) => express()
   .use(express.static("public"))
   .get("/serviceworker/redirect.html", (_, res) => res.status(OK).sendFile(appRootPath + '/node_modules/@toruslabs/torus-direct-web-sdk/serviceworker/redirect.html'))
   .get("/serviceworker/sw.js", (_, res) => res.status(OK).sendFile(appRootPath + '/node_modules/@toruslabs/torus-direct-web-sdk/serviceworker/sw.js'))
-  .get("/torus", app());
+  .get("/torus", app(...args));
