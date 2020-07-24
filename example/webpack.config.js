15d37a1d3469863dd24d14f6be70dec7f2097bcd
Alex Thomas <hello@cawfree.com>
2020-07-24 15:36:58 +0100

    chore: add example

---
+++ example/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+
+module.exports = {
+  //mode: process.env.NODE_ENV,
+  plugins: [],
+  entry: {
+    vendor: ["@babel/polyfill", "react"],
+    app: ["./src/app/index.js"]
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json", ".wasm", ".mjs", "*"]
+  }
+};
