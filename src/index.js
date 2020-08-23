c64b72d08b2210f830acfaf4e0440e351732e013
Alex Thomas <hello@cawfree.com>
2020-08-23 22:23:35 +0100

    chore: update viewport scale

--- src/index.js
+++ src/index.js
@@ -49,6 +49,7 @@ const app = ({
 <html>
   <head>
     <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Torus Login</title>
     <script>
       /* extern */
